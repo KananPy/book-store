c8fd96c2bb114e52f201c6ffdbb28a659e3ad7ec
sabuhi <msebuhi040@gmail.com>
2023-06-29 00:50:58 +0400

    I solved Join and Contact Tasks for admin Pages

---
+++ src/js/convertDataObj.js
@@ -0,0 +1,10 @@
+function convertDataObj(obj){
+    const array = Object.entries(obj)
+    const newArray= array.map((item) =>({
+        id:item[0],
+        ...item[1]
+   
+       
+    }))
+    return newArray
+}
